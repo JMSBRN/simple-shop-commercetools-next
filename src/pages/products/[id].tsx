1294f4aaaa6b6d95045db362009fe78709b1b8cd
zakhavai@gmail.com <zakhavai@gmail.com>
2023-08-23 19:46:12 +0300

    fix: move products render logic to the server side

--- src/pages/products/[id].tsx
+++ src/pages/products/[id].tsx
@@ -1,28 +1,16 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Product } from '@commercetools/platform-sdk';
+import { Category, Product } from '@commercetools/platform-sdk';
+import { getCategories, getProductsByCategoryId } from '@/commercetools/utilsCommercTools';
+import { GetStaticProps } from 'next';
 import ProductCard from '@/components/product-card/ProductCard';
-import { getProductsByCategoryId } from '@/commercetools/utilsCommercTools';
+import React from 'react';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import styles from '../../styles/Products.module.scss';
 import { useRouter } from 'next/router';
 
-function Products() {
-  const { push, query } = useRouter();
+function Products({ products }: {products: Product[]}) {
+  const { push } = useRouter();
   const { productsContainer, productsStyle, productInfoStyle } = styles;
-  const [products, setProducts] = useState<Product[]>([] as Product[]);
-  const { id } = query;
   
-  const fetchFn = useCallback(async () => {
-    if(typeof id === 'string') {
-      const res = await getProductsByCategoryId(id);
- 
-      if(res) setProducts(res);
-    }
-  }, [id]);
-
-  useEffect(() => {
-  fetchFn();
-  },[fetchFn]);
-
   const handleGetProductInfo = (id: string) => {
     push(`/product-info/${id}`);
   };
@@ -46,5 +34,31 @@ function Products() {
     </div>
   );
 }
+export const getStaticPaths = async ({ locales }: { locales: string[] }) => {
+  const categories = await getCategories() as Category[];
+  const paths = categories.flatMap((el) => 
+   locales?.map((locale) => ({
+      params: {
+        id: el.id
+      },
+      locale
+   }))
+  );
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export default Products;
+export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
+  const products = await getProductsByCategoryId(params?.id as string);
+
+  return {
+    props: {
+      products,
+      ...(await serverSideTranslations( locale || 'en', ['translation', 'common']))
+    },
+  };
+};

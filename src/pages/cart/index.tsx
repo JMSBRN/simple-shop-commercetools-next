6eab86a262732ab4622ed026b8564a8e10b66312
JMSBRN <zakhavai@gmail.com>
2023-09-07 12:52:58 +0300

    feat: created Cart component.

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,10 +2,48 @@ import React from 'react';
 import styles from '../../styles/Cart.module.scss';
 
 function Cart() {
-    const { cartContiner } = styles;
+    const { 
+      cartContiner,
+      cartTitle,
+      mainContainer,
+      leftSideContainer,
+      lineItems,
+      lineItem,
+      promoCodeContainer,
+      cartTotalsContainer,
+      cartTotalsTable,
+      cartTotalsTitle,
+      cartTotalsInfo,
+      cartTotals,
+
+     } = styles;
 
   return (
-    <div className={cartContiner}>cart</div>
+    <div className={cartContiner}>
+      <div className={cartTitle}>Customer Cart</div>
+      <div className={mainContainer}>
+        <div className={leftSideContainer}>
+        <div className={lineItems}>
+          <div className={lineItem}></div>
+          <div className={lineItem}></div>
+          <div className={lineItem}></div>
+          </div>
+         <div className={promoCodeContainer}>
+          <input type="text" />
+          <button type="button">Apply</button>
+         </div>
+        </div>
+          <div className={cartTotalsContainer}>
+            <div className={cartTotalsTable}>
+              <div className={cartTotalsTitle}>Cart Totals</div>
+              <div className={cartTotalsInfo}>Sub Totals<span>130,00</span></div>
+              <div className={cartTotalsInfo}>Tax Totals<span>23,00</span></div>
+              <div className={cartTotals}>Total :<span>123,00</span></div>
+            </div>
+            <button type="button">Checkout</button>
+            </div>   
+      </div>
+    </div>
   );
 };
 

5a19f84d1bd61572bc93559c61f21cad81ec5bf9
zakhavai@gmail.com <zakhavai@gmail.com>
2023-09-15 19:30:28 +0300

    fix: fixed currency in productInfo comp.

--- src/components/product-info/ProductInfo.tsx
+++ src/components/product-info/ProductInfo.tsx
@@ -14,45 +14,67 @@ function ProductInfo({ product }: { product: Product }) {
     productInfoContainer,
     variantsStyle,
     variantStyle,
-    addToCardContainerStyle
+    addToCardContainerStyle,
   } = styles;
   const { current, staged } = product.masterData;
   const { masterVariant, variants } = current;
   const { name } = staged;
   const { prices } = masterVariant;
-  const currency = prices?.find(el => el.value.currencyCode)?.value.currencyCode;
-  const { language } = useAppSelector(selectCommerceTools);
+  const { language, country } = useAppSelector(selectCommerceTools);
+  const currency = prices
+    ?.filter((el) => el.country === country)
+    ?.find((el) => el.value.currencyCode)?.value.currencyCode!;
   const productName = filterObjectAndReturnValue(name, language);
-  const [currentVariants, setCurrentVariants] = useState<ProductVariant[]>(variants);
+  const [currentVariants, setCurrentVariants] =
+    useState<ProductVariant[]>(variants);
   const handleSelectVariant = (id: number) => {
-     setSelectedIdVariant(id);
-     if(id !== 1) {
-      setCurrentVariants([...variants, masterVariant ]);
-     } else { 
+    setSelectedIdVariant(id);
+    if (id !== 1) {
+      setCurrentVariants([...variants, masterVariant]);
+    } else {
       setCurrentVariants(variants);
-     } 
+    }
   };
 
   return (
     <div className={productInfoContainer}>
-      { selectedIdVariant === 1 ?
-       (<MasterVariant productName={productName!} masterVariant={masterVariant} />)
-      : (<>{ variants.filter((el) => el.id === selectedIdVariant).map((el) => (
-        <MasterVariant key={el.id}  masterVariant={el} productName={productName!} />
-      ))}</>)}
+      {selectedIdVariant === 1 ? (
+        <MasterVariant
+          productName={productName!}
+          masterVariant={masterVariant}
+        />
+      ) : (
+        <>
+          {variants
+            .filter((el) => el.id === selectedIdVariant)
+            .map((el) => (
+              <MasterVariant
+                key={el.id}
+                masterVariant={el}
+                productName={productName!}
+              />
+            ))}
+        </>
+      )}
       <div className={addToCardContainerStyle}>
-        <AddToCard productId={product.id} variantId={masterVariant.id} currency={currency!} />
+        <AddToCard
+          productId={product.id}
+          variantId={masterVariant.id}
+          currency={currency!}
+        />
       </div>
       <div className={variantsStyle}>
-        {currentVariants.filter((el) => el.id !== selectedIdVariant).map((el) => (
-          <div 
-          className={variantStyle}
-          key={el.id}
-          onClick={() => handleSelectVariant(el.id)}
-          >
-            <ProductCardVariant  variant={el} />
-          </div>
-        ))}
+        {currentVariants
+          .filter((el) => el.id !== selectedIdVariant)
+          .map((el) => (
+            <div
+              className={variantStyle}
+              key={el.id}
+              onClick={() => handleSelectVariant(el.id)}
+            >
+              <ProductCardVariant variant={el} />
+            </div>
+          ))}
       </div>
     </div>
   );

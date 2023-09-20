88433e3429ae0f20ab18c2a7e0c3bb59951ce8cd
JMSBRN <zakhavai@gmail.com>
2023-09-20 15:52:00 +0300

    fix: updated CustomerCart comp.

--- src/components/cart/CustomerCart.tsx
+++ src/components/cart/CustomerCart.tsx
@@ -1,14 +1,12 @@
 import { Cart, TaxedPrice } from '@commercetools/platform-sdk';
 import CartLineItem from './cart-line-item/CartLineItem';
 import Link from 'next/link';
-import React, {  } from 'react';
-import {
-  getMoneyValueFromCartField,
-} from '@/commercetools/utils/utilsCarts';
+import React from 'react';
+import { getMoneyValueFromCartField } from '@/commercetools/utils/utilsCarts';
 import styles from './CustomerCart.module.scss';
 import { useRouter } from 'next/router';
 
-function CustomerCart({ cart }:{ cart: Cart }) {
+function CustomerCart({ cart }: { cart: Cart }) {
   const {
     cartContainer,
     cartTitle,
@@ -20,22 +18,26 @@ function CustomerCart({ cart }:{ cart: Cart }) {
     cartTotalsTable,
     cartTotalsTitle,
     cartTotalsInfo,
+    cartTotalsInfoData,
     cartTotals,
     lineItemHeadlines,
   } = styles;
 
   const { push } = useRouter();
-  
+
   const handleCheckout = async () => {
-    if(cart?.lineItems.length) {
+    if (cart?.lineItems.length) {
       push(`/checkout/${cart?.id}`);
     }
   };
 
-    const { taxPortions, totalGross, totalNet, totalTax } = cart.taxedPrice as TaxedPrice;
+  const { taxedPrice, shippingInfo } = cart;
+  const { taxPortions, totalGross, totalNet, totalTax } =
+    taxedPrice as TaxedPrice;
+  const shipingMethodTaxTotal = shippingInfo?.taxedPrice?.totalGross!;
 
-    return (
-      <div className={cartContainer}>
+  return (
+    <div className={cartContainer}>
       <div className={cartTitle}>
         <h3>Customer Cart</h3>
         <Link href={'/'}>Home</Link>
@@ -60,7 +62,7 @@ function CustomerCart({ cart }:{ cart: Cart }) {
             ))}
           </div>
           <div className={promoCodeContainer}>
-            <input type="text" placeholder='promo code' />
+            <input type="text" placeholder="promo code" />
             <button type="button">Apply</button>
           </div>
         </div>
@@ -68,28 +70,34 @@ function CustomerCart({ cart }:{ cart: Cart }) {
           <div className={cartTotalsTable}>
             <div className={cartTotalsTitle}>Cart Totals</div>
             <div className={cartTotalsInfo}>
-              Net amount
-              <span>
-              {getMoneyValueFromCartField(totalNet)}
-              </span>
-            </div>
-            <div className={cartTotalsInfo}>
-              <div>{taxPortions.map((el, idx) => (
-                <span
-                 key={idx}>
-                  <span>{el.name} </span>
-                  <span>{`${(el.rate * 100)} %`}</span>
-                </span>
-              ))}</div>
-              <span>
-              { totalTax && getMoneyValueFromCartField(totalTax)}
-              </span>
+              Taxes included
+              <div className={cartTotalsInfoData}>
+                Net amount
+                <span>{getMoneyValueFromCartField(totalNet)}</span>
+              </div>
+              <div className={cartTotalsInfoData}>
+                <div>
+                  {taxPortions.map((el, idx) => (
+                    <span key={idx}>
+                      <span>{el.name} </span>
+                      <span>{`${el.rate * 100} %`}</span>
+                    </span>
+                  ))}
+                </div>
+                <span>{getMoneyValueFromCartField(totalTax!)}</span>
+              </div>
+              <div className={cartTotalsInfoData}>
+                Delivery
+                <span>{getMoneyValueFromCartField(shipingMethodTaxTotal)}</span>
+              </div>
             </div>
             <div className={cartTotals}>
-              Total :  {getMoneyValueFromCartField(totalGross)}
+              Total : {getMoneyValueFromCartField(totalGross)}
             </div>
           </div>
-          <button type="button" onClick={handleCheckout}>Checkout</button>
+          <button type="button" onClick={handleCheckout}>
+            Checkout
+          </button>
         </div>
       </div>
     </div>

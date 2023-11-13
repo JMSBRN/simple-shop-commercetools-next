4a442b1bfd1a1a271b5e38452986a7874e96d425
zakhavai@gmail.com <zakhavai@gmail.com>
2023-11-13 22:14:04 +0300

    fix: translated footer.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,9 +4,11 @@ import Link from 'next/link';
 import React from 'react';
 import socialMediaImages from '../../../public/data/social-media-images.json';
 import styles from './Footer.module.scss';
+import { useTranslation } from 'next-i18next';
 
 const Footer = () => {
   const { footer, logoStyle, container, section, socialLinks } = styles;
+  const { t } = useTranslation('common');
 
   return (
     <footer className={footer}>
@@ -17,15 +19,15 @@ const Footer = () => {
             <div>international</div>
           </div>
           <p>
-            Buy everywhere, buy cheaper, receive at your address and enjoy..
+           {t('shopSlogan')}
           </p>
         </div>
         <div className={section}>
-          <h3>Explore</h3>
+          <h3>{t('explore')}</h3>
           <Categories />
         </div>
         <div className={section}>
-          <h3>Connect with Us</h3>
+          <h3>{t('contactUsMessage')}</h3>
           <div className={socialLinks}>
             {socialMediaImages['social-media-links'].map((el) => (
               <div key={el.name}>

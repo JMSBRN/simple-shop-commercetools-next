b98aa7a95e47229e9a553391f816bc52c34b4f8d
zakhavai@gmail.com <zakhavai@gmail.com>
2023-11-16 17:16:11 +0300

    fix: removed loader from header comp.

--- src/components/categories/Categories.tsx
+++ src/components/categories/Categories.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '@/hooks/storeHooks';
 import { Category } from '@commercetools/platform-sdk';
-import Loader from '../loader/Loader';
 import { fetchCategories } from '@/features/thunks/FetchCategories';
 import { filterObjectAndReturnValue } from '@/commercetools/utils/utilsCommercTools';
 import { isInternetConnectionOnline } from '@/commercetools/utils/utilsApp';
@@ -35,15 +34,14 @@ function Categories() {
           You are offline. Please check your internet connection.
         </div>
       )}
-      {status === 'loading' && isOnline && <Loader />}
-      {categories
+      {status ==='succeeded' && categories
         .filter((el) => el.parent === undefined)
         .map((el) => (
           <div key={el.id} onClick={() => handleClickOnCategories(el)}>
             <p>{filterObjectAndReturnValue(el.name, language)}</p>
           </div>
         ))}
-      {userName && (
+      {(userName && status ==='succeeded') && (
         <div className={dashBoardLInk} onClick={() => push('/user/dashboard')}>
           {t('dashBoard')}
         </div>
